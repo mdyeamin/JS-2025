a57ee746002d81d99af9cb8fbc935b5d5e9767fb
md yeamin <ahmedyeamin05@gmail.com>
2024-12-09 18:16:51 +0600

    logical not

---
+++ logical_not.js
@@ -0,0 +1,36 @@
+// logical yes operator 
+let isLove = true;
+
+// if (islove === true) use cases possibilitys
+if (isLove){  // true value justify 
+console.log("love");
+}
+else{
+    console.log("hate" ); 
+}
+
+// logical not operator
+
+let isLeader  = true;
+
+if (!isLeader) {
+console.log("you can't visit");
+    
+}
+else{
+    console.log("you can visit");
+    
+}
+
+// logical duble not operator
+
+let isLeaderShip  = true;
+
+if (!!isLeaderShip) {
+console.log("you can visit");
+    
+}
+else{
+    console.log("you can't visit");
+    
+}
