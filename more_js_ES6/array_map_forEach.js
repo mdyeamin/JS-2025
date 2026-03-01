6c6031c3da2af651d262a2bff894edfb4c66401b
mdyeamin <ahmedyeamin05@gmail.com>
2026-03-01 21:22:34 +0600

    array map with arrow function different way

--- more_js_ES6/array_map_forEach.js
+++ more_js_ES6/array_map_forEach.js
@@ -19,7 +19,7 @@ const doubled = items.map(doubleIt);
 // another way to do the same thing is using map
 // method with inline arrow function
 const doubleIt2 = items.map(num => num * num); 
-console.log(doubleIt2);
+// console.log(doubleIt2);
 
 
 
@@ -27,7 +27,28 @@ const friends = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "H
 
 // using for loop to get the first 5 friends
 const firstFiveFriends = friends.slice(0, 5);
-console.log(firstFiveFriends);
+// console.log(firstFiveFriends);
 
 const firstLetters =  friends.map(friend => friend[0]);
-console.log(firstLetters);
+// console.log(firstLetters);
+
+
+const products = [
+    { name: "Laptop", price: 999 },
+    { name: "Smartphone", price: 499 },
+    { name: "Tablet", price: 299 },
+    { name: "Headphones", price: 199 },
+    { name: "Smartwatch", price: 199 },
+    { name: "Camera", price: 599 }
+];
+
+const prices = products.map((product, index, allArray)=> { 
+    // index and allArray are optional parameters in map method
+    // it will give you the index of the current element and the whole array respectively
+    const discountedPrice = product.price * 0.9; // Apply a 10% discount
+    
+    
+    return discountedPrice;
+});
+
+console.log(prices);
