8f1046eeeec25d7eb43772c0c5742285a06fdb0e
mdyeamin <ahmedyeamin05@gmail.com>
2026-03-04 19:18:06 +0600

    todo APIs

--- APIs/web[api]/app.js
+++ APIs/web[api]/app.js
@@ -4,17 +4,28 @@
 //   .then((data) => console.log(data));
 
 const url = "https://jsonplaceholder.typicode.com/posts";
-
-document.getElementById("dataLoad").addEventListener("click", () => {
+const fetchPost = ()=>{
   fetch(url)
     .then((res) => res.json())
 // .then(data=> loadData(data))
     .then(loadData); // এভাবেও কল করা যায়, কোন সমস্যা নাই
-});
+}
+document.getElementById("dataLoad").addEventListener("click", () => fetchPost());
 
 function loadData(posts) {
+  // get the container 
+  const container = document.getElementById("card-container")
+  container.innerHTML = ""; // clear the container before adding new cards
   posts.forEach(element => {
-    console.log(element.title);
-    
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class="card">
+        <h3>${element.title}</h3>
+        <p>${element.body}</p>
+    </div>
+    `
+    container.append(div)
   });
+
 }
+fetchPost()
