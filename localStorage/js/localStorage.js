3d0a140d407635d3b1e9d09ce5b9eea30013f69b
mdyeamin <ahmedyeamin05@gmail.com>
2026-03-19 22:39:21 +0600

    local storage example updated

--- localStorage/js/localStorage.js
+++ localStorage/js/localStorage.js
@@ -11,11 +11,9 @@ const gerNumberFromLS = () => {
 };
 
 const addProduct = () => {
-  const quantity = document.getElementById("quantity").value;
   const product = document.getElementById("product").value;
-  const disabled = document.getElementById("addProduct");
-  const div = document.getElementById("ShowProduct");
-  div.innerHTML = "";
+  const quantity = parseInt(document.getElementById("quantity").value);
+
   // ShowProduct(product, quantity);
   storedFromCart(product, quantity);
 
@@ -23,35 +21,40 @@ const addProduct = () => {
   document.getElementById("quantity").value = "";
 };
 
-const cart = {};
-
 const createCart = () => {
-  // return JSON.parse(localStorage.getItem("cart")) || {};
-  const data = localStorage.getItem("cart");
-  return data ? JSON.parse(data) : {};
+  const cart = {};
+  // return JSON.parse(localStorage.getItem("cart")) || {}; // evabew kora jay
+  const data = localStorage.getItem("cart"); // evabew kora jay
+  return data ? JSON.parse(data) : cart;
 };
 
 const storedFromCart = (product, quantity) => {
   const cart = createCart();
-  cart[product] = quantity;
+  if (cart[product]) {
+    cart[product] += quantity;
+  } else {
+    cart[product] = quantity;
+  }
+
+
   localStorage.setItem("cart", JSON.stringify(cart));
   ShowProduct(cart);
 };
 
 const ShowProduct = (cart) => {
+  const div = document.getElementById("ShowProduct");
+  div.innerHTML = "";
   for (const item in cart) {
     const quantity = cart[item];
-    console.log(item, quantity);
 
     const ul = document.createElement("ul");
-
     ul.innerHTML = `
                 <li class="${item || quantity ? "flex" : "hidden"} items-center justify-between p-4 bg-slate-50 rounded-xl border border-slate-100 group hover:border-[#6610f2] transition-colors">
                     <span class="font-bold text-slate-700">${item}</span>
                     <span class="bg-indigo-100 text-[#6610f2] px-3 py-1 rounded-lg font-bold text-sm">${quantity} ${quantity ? "Unit" : ""}</span>
                 </li>
 `;
-    const div = document.getElementById("ShowProduct");
+
     div.append(ul);
   }
 };
